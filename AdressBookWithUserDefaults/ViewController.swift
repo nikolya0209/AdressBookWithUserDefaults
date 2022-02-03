a1454ce27fc9fef55e854348e3a5b08e4f806de0
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-02-03 20:49:11 +0300

    UI

--- AdressBookWithUserDefaults/ViewController.swift
+++ AdressBookWithUserDefaults/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var streetField: UITextField!
+    @IBOutlet weak var houseField: UITextField!
+    @IBOutlet weak var buildField: UITextField!
+    @IBOutlet weak var flatField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func saveAction(_ sender: Any) {
+        
+        
+    }
+    
 }
 
